2ddfa01c63cd36d09108106d847fb34377006356
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-10-16 21:38:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,24 @@
  */
 void *_strcat(char *dest, char *src)
 {
-	int l1, i, zl2, len;
-        l1 = l2 = 0;
-        while (dest[l1] != '\0')
-        {
-                l1++;
-        }
-        while (src[l2] != '\0')
-        {
-                l2++;
-        }
-        l2 = len;;
-	
-	while (i = 0; i <= len; i++)
+	int l1, l2, len;
+
+	l1 = l2 = 0;
+	while (dest[l1] != '\0')
 	{
-		dest[l1 + i] = src[i];
+		l1++;
+	}
+	while (src[l2] != '\0')
+	{
+		l2++;
+	}
+	len = l2;
+	l2 = 0;
+	while (l2 < len)
+	{
+		dest[l1] = src[l2];
+		l1++;
+		l2++;
 	}
 	return (dest);
 }
